b484f3a556b592b8653e2763f34c0cb1f36a736a
ricardobad51 <rbbelem@gmail.com>
2021-06-17 00:20:21 -0300

    Backend project created

---
+++ backend/src/main/java/com/ricardoburitibelem/clientcatalog/ClientcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.ricardoburitibelem.clientcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientcatalogApplication.class, args);
+	}
+
+}
